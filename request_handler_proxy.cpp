c587719b2cd868cccfd3f28dc84673c7ea0a959f
Ryan Peterman <ryanlpeterman@gmail.com>
2017-03-01 19:18:31 -0800

    fixed gitignore, added minimal proxy

--- request_handler_proxy.cpp
+++ request_handler_proxy.cpp
@@ -4,6 +4,17 @@ namespace http {
 namespace server {
 
 	RequestHandler::Status ProxyHandler::Init(const std::string& uri_prefix, const NginxConfig& config) {
+		for (auto it = config.statements_.begin(); it != config.statements_.end(); it++) {
+
+			// if current statement has two tokens
+			if ((*it)->tokens_.size() == 2) {
+				if ((*it)->tokens_[0] == "port") {
+					port = (*it)->tokens_[1];
+				} else if ((*it)->tokens_[0] == "host") {
+					host = (*it)->tokens_[1];
+				}
+			}
+		}
 
 		return RequestHandler::Status::OK;
 	}
@@ -11,6 +22,7 @@ namespace server {
 
 	RequestHandler::Status ProxyHandler::HandleRequest(const Request& request, Response* response) {
 	  	std::cout << "ProxyHandler::HandleRequest called" << std::endl;
+	  	std::cout << "Making request to: http://www." << host << ":" << port << std::endl;
 
 	  	response->SetStatus(Response::ResponseCode::OK);
 	  	response->AddHeader("Content-Type", "text/plain");
--- request_handler_proxy.h
+++ request_handler_proxy.h
@@ -2,6 +2,7 @@
 #define PROXY_HANDLER_H
 
 #include "request_handler.h"
+#include "response_parser.h"
 #include <iostream>
 
 namespace http {
@@ -11,8 +12,15 @@ namespace http {
 			Status Init(const std::string& uri_prefix, const NginxConfig& config) override;
 
   			Status HandleRequest(const Request& request, Response* response) override;
-		};
+		private:
+			ResponseParser resp_parser;
 
+			// domain name of host to proxy
+			std::string host;
+
+			// port num as a string
+			std::string port;
+		};
 
 		REGISTER_REQUEST_HANDLER(ProxyHandler);
 	} // namespace server
